430114a7afaa88c965843d75c1eb8f4a1da50b06
vineethkunnathsg@gmail.com <vineethkunnathsg@gmail.com>
2018-07-17 21:44:08 +0800

    completed crud functions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,7 +7,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#clear the table before we seed
 Pokemon.destroy_all
 
 json = ActiveSupport::JSON.decode(File.read('db/pokedex.json'))
